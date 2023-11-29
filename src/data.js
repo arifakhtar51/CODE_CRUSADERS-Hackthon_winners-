5df23fe5560e0a3bdc4334cce99291332308f331
arifakhtar51 <arifakhtar902@gmail.com>
2023-11-30 00:50:34 +0530

    my_team_CODE_CRUSADERS

---
+++ src/data.js
@@ -0,0 +1,32 @@
+const reviews = [
+  {
+    id: 1,
+    name: "ARIF AKHTAR",
+    job: "Backend-Developer",
+    image: "https://i.postimg.cc/Jn9Bh4Qy/arif.jpg",
+    text: "As the Backend Developer for our award-winning blockchain project, I played a crucial role in architecting and implementing the robust infrastructure that powers our decentralized application. The creativity and attention to detail within our team were instrumental in securing our victory in the hackathon. It's been an incredible journey, and I'm proud to be part of a team that consistently delivers exceptional results.",
+  },
+  {
+    id: 2,
+    name: "ANSH SHARMA",
+    job: "Software Engineer",
+    image: "https://i.postimg.cc/LXrNMSYD/ansh.jpg",
+    text: "In my role as a Software Engineer, I contributed to the success of our blockchain project by leveraging my technical expertise to design and implement innovative solutions. Our project not only met but exceeded the hackathon criteria, showcasing our ability to deliver quality solutions on time and within budget. Working with this team has been a rewarding experience, and I look forward to continuing our journey of excellence.",
+  },
+  {
+    id: 3,
+    name: "SUMIT KUMAR",
+    job: "BlockChain Developer",
+    image: "https://i.postimg.cc/Y9jYxTPN/sumit.png",
+    text: "As a Blockchain Developer, I am thrilled to share the success of our recent victory in the hackathon. Our blockchain project demonstrated the power and potential of decentralized technology. The hard work and dedication of the entire team were instrumental in achieving this milestone. I'm excited about the positive impact our project can make in the world of blockchain and beyond.",
+  },
+  {
+    id: 4,
+    name: "BHAVYA CHAUHAN",
+    job: "Content Strategist",
+    image: "https://i.postimg.cc/7h4DYpj1/bhavyanew-Edited.jpg",
+    text: "As a Content Strategist, I had the privilege of crafting compelling narratives that communicated the significance and impact of our blockchain project. This experience has been unique and inspiring, working with a team that not only excels in technical prowess but also understands the importance of effective communication. Our victory in the hackathon is a testament to the seamless integration of technology and storytelling, and I'm proud to have been part of this journey.",
+  }
+];
+
+export default reviews;
